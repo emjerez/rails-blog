cf3b1499ca3be4903e255bf6ff6bfb851ed3e37e
Ethel Jerez <ethelmj@gmail.com>
2018-03-13 12:26:35 -0400

    adding all files

---
+++ config/routes.rb
@@ -0,0 +1,14 @@
+Rails.application.routes.draw do
+  get 'welcome/index'
+  resources :articles do
+    resources :comments
+  end
+  root 'welcome#index'
+  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+end
+
+# root 'welcome#index' tells Rails to map requests to
+# the root of the application to the welcome controller's index
+# action and get 'welcome/index' tells Rails to map
+# requests to http://localhost:3000/welcome/index to the welcome
+# controller's index action.
